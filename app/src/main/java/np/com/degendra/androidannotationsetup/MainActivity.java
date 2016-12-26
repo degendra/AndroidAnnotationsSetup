fc2fe814516fc0512cf05d6b4786e890005f5c17
Degendra Sivakoti <degendra@Macs-MacBook-Pro.local>
2016-12-26 21:51:07 +0545

    initial commit

---
+++ app/src/main/java/np/com/degendra/androidannotationsetup/MainActivity.java
@@ -0,0 +1,28 @@
+package np.com.degendra.androidannotationsetup;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+    @ViewById
+    Toolbar toolbar;
+
+    @AfterViews
+    void afterViews() {
+        setSupportActionBar(toolbar);
+    }
+
+    @Click(R.id.fab)
+    void onFabClicked(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+}
